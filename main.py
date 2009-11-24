db397a9d9b4ded0aaaf547abdabdb65d1138b39d
James Stoker <jstoker@jcs.me.uk>
2009-11-24 03:15:05 +0000

    Pseudo-User can now join a channel, /msg Pseudo-User #channel No part function, or any access control... But, It joins (without TS0 problem)

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ class ServerLink (threading.Thread):
 	def __init__ (self):
 		threading.Thread.__init__ (self)
 		self.sock = sock.Socket()
-		self.uuid = {}
 	
 	def connect (self):
 		self.sock.connect (config.link_remote, config.link_port)
@@ -21,20 +20,21 @@ class ServerLink (threading.Thread):
 		# Adding Client
 		uuid = config.link_sid + uid
 		self.sock.ssend ('UID ' + uuid + ' ' + str(currenttime()) + ' ' + nick + ' ' + host + ' ' + host + ' ' + ident + ' 0.0.0.0 ' + str(currenttime()) + ' +' + modes + ' :' + gecos)
-		self.uuid[uuid] = [nick + '!' + ident + '@' + host, {}]
-		for mode in modes:
-			self.uuid [uuid][1][mode] = True
 		# Opering..
 		self.sock.usend (uid ,'OPERTYPE %s' % opertype)
+
+	def channel_join (self, bot, channel, modes='oa'):
+		self.sock.ssend ('FJOIN %s %s + :%s,%s' % (channel, currenttime (), modes, bot))
 
 	def run (self):
 		while 1:
 			data = self.sock.receive (2048)
 			lines = data.split ('\r\n')
 			for line in lines:
-				if line.startswith( 'Link is not connected'):
+				if line.startswith( 'Link is not connected'): # Socket has been dropped. Reconnecting.
 					time.sleep (2)
 					self.connect()
+
 				if line == '':
 					break
 				
@@ -56,47 +56,12 @@ class ServerLink (threading.Thread):
 				if words[1] == 'PING':
 					self.sock.ssend ('PONG %s' % words[3])
 				
-				if words[1] == 'UID':
-					uuid = words[2]
-					useridenthostmask = words[4] + '!' + words[7] + '@' + words[5]
-					self.uuid[uuid] = [useridenthostmask, {}]
-					for mode in words[10][1:]:
-						self.uuid [uuid][1][mode] = True
-
-				if words[1] == 'MODE':
-					pass
-					uuid = words[2]
-					mode = words[3]
-					self.user_mode_change (uuid, mode)
-
 				if words[1] == 'PRIVMSG':
-					user = words[0][1:]
-					#print self.user_umode(user, 'o')
-					print self.uuid[user][1]
-
+					self.channel_join (words[2], words[3][1:])
+				
 				if words[0] == 'ERROR':
 					print 'Fatal Error Occurred'
 					raise SystemExit
-
-	def user_mode (self, user, mode):
-		if self.uuid[user][1][mode]:
-			return True
-		else:
-			return False
-	
-	def user_mode_change (self, user, mode):
-		modes = re.findall ('[+-][a-zA-Z]+', mode)
-		for mode in modes:
-			
-			if mode[0] == '+':
-				for char in modes:
-					self.uuid[user][1][char[1]] = True
-			else:
-				for char in modes:
-					self.uuid[user][1][char[1]] = False
-	
-	def get_user (self, uuid):
-		return self.uuid[uuid][0]
 
 if __name__ == '__main__':
 	link = ServerLink()
