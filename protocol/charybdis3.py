15069cc8c51d95c89e04c006185b70148718ca88
jstoker <jstoker@athena.jcs.me.uk>
2010-06-17 00:40:06 +0000

    Charybdis protocol module fix.

--- protocol/charybdis3.py
+++ protocol/charybdis3.py
@@ -8,7 +8,7 @@ class protocol:
 		self.conf = self.sock.conf
 
 	def connect (self):
-		self.send ('PASS %s TS 6 %s' % (self.conf['recvpass'], self.conf['sid']))
+		self.send ('PASS %s TS 6 %s' % (self.conf['pass'], self.conf['sid']))
 		self.send ('CAPAB :QS EX IE KLN UNKLN ENCAP TB SERVICES EUID EOPMOD')
 		self.send ('SERVER %s 1 :Pseudo-Server - http://github.com/jstoker/pseudo-server' % self.conf['server'])
 	
